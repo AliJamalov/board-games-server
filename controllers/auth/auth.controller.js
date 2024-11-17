888d297a92b44f58d338b372028fd9ef324bb02d
AliJamalov <168686371+AliJamalov@users.noreply.github.com>
2024-11-17 04:32:01 +0400

    add sameite

--- controllers/auth/auth.controller.js
+++ controllers/auth/auth.controller.js
@@ -101,6 +101,7 @@ export const login = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: true,
+      sameSite: "none",
     });
 
     res.status(200).json({
